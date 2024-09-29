74c2b164408be40bcc80d2d446b7c185ac8df7f8
HedgeHogSE <sasha.ezhak2006@gmail.com>
2024-09-29 21:54:09 +0400

    Added constructor with Human father

--- src/university/oop/ex1_2/Human.java
+++ src/university/oop/ex1_2/Human.java
@@ -6,10 +6,17 @@ public class Human
     public FullName fullName;
     public int height;
 
+    Human father;
 
-    public Human(FullName fullName, int height)
-    {
+    public Human(FullName fullName, int height) {
         this.fullName = fullName;
+        this.height = height;
+    }
+    public Human(FullName fullName, int height, Human father) {
+        this.fullName = fullName;
+        this.father = father;
+        if (this.fullName.surname == null) this.fullName.surname = this.father.fullName.surname;
+        if (this.fullName.patronymic == null) this.fullName.patronymic = this.father.fullName.name + "ович";
         this.height = height;
     }
 
